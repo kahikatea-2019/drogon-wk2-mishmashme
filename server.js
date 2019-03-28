07758df093c7ff2bb75dae3b23bbdacf6f0b8b11
Bradley Wheeler <bradley.wh@outlook.com>
2019-03-28 17:28:09 +1300

    Inital setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const server = express()
+
+// Exports
+module.exports = server
+
+// Routers
+const router = require('./routes')
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Routing
+server.use('/', router)

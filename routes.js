62ad6a54da5933106813fe97542d266fb6a3069a
Struan Wall <struan03@hotmail.com>
2019-03-29 15:32:54 +1300

    Working 3 images

--- routes.js
+++ routes.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const router = express.Router()
-// const data = require('./data.json')
+const data = require('./data.json')
 // const fs = require('fs')
 
 module.exports = router
 
 router.get('/', (req, res) => {
-  res.render('partials/home')
+  res.render('home')
 })
 
 router.get('/results', (req, res) => {
-  res.render('partials/results')
+  const inputData = req.query.Celeb
+
+  const celeb = data.celebs.find(i => i.name === inputData)
+
+  res.render('results', celeb)
 })
 
-router.post('/', (req, res) => {
-  const inputData = req.body
-  // res.send(inputData)
-  if (inputData.Celeb === 'Kanye') {
-    res.redirect('/results')
-  }
-})
+// router.post('/', (req, res) => {
+//   const inputData = req.body
+//   // res.send(inputData)
+//   if (inputData.Celeb === 'Kanye') {
+//     res.redirect('/results')
+//   }
+// })
+
+// router.post('/', (req, res) => {
+//  const inputData = req.body
+// if (inputData.Celeb === 'Kanye') {
+// go to data.json celebs object === kanye
+// display image1
+// data.celebs.find
+
+//     res.redirect('/results')
+//   }
